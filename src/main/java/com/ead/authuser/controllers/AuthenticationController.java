ea17dff6ed43a43f88d5063e08164787db01be67
Yane Wanderley <yane.rodrigues@ufpe.br>
2024-03-21 21:26:24 -0300

    Principais Bibliotecas de Logging, Log Levels e Logging no Spring com Logback

--- src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -3,12 +3,15 @@ package com.ead.authuser.controllers;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +28,8 @@ import com.fasterxml.jackson.annotation.JsonView;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/auth")
 public class AuthenticationController {
+	
+	Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
 	
 	@Autowired
 	UserService userService;
@@ -46,9 +51,18 @@ public class AuthenticationController {
 		userModel.setUserType(UserType.STUDENT);
 		userModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
 		userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
-		userService.save(userModel);
+		this.userService.save(userModel);
 		return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
 	}
 	
+	@GetMapping("/")
+	public String index() {
+		this.logger.trace("TRACE");
+		this.logger.debug("DEBUG");
+		this.logger.info("INFO");
+		this.logger.warn("WARN");
+		this.logger.error("ERROR");
+		return "Testando Logging";
+	}
 
 }
