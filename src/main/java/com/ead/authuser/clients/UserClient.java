8263c627a69eb73e12afb21feac0eab639debae0
Yane Wanderley <yane.rodrigues@ufpe.br>
2024-03-27 20:50:40 -0300

    API Composition Pattern em Authuser Microservice

---
+++ src/main/java/com/ead/authuser/clients/UserClient.java
@@ -0,0 +1,52 @@
+package com.ead.authuser.clients;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+
+import com.ead.authuser.dtos.CourseDto;
+import com.ead.authuser.dtos.ResponsePageDto;
+import com.ead.authuser.services.UtilsService;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Component
+public class UserClient {
+
+	@Autowired
+	RestTemplate restTemplate;
+	
+	@Autowired
+	UtilsService utilsService;
+	
+	
+	public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable){
+		List<CourseDto> searchResult = null;
+		String url = utilsService.createUrl(userId, pageable);
+		log.debug("Request URL: {}", url);
+		log.info("Request URL: {}", url);
+		
+		try {
+			ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType = new ParameterizedTypeReference<ResponsePageDto<CourseDto>>() {}; 
+			ResponseEntity<ResponsePageDto<CourseDto>> result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+			searchResult = result.getBody().getContent();
+			log.debug("Response number of elements: {}", searchResult.size());
+		}catch(HttpStatusCodeException e ) {
+			log.error("Error request /coursed {}", e);
+		}
+		log.info("Ending request /courses userId {}", userId);
+		return new PageImpl<>(searchResult);
+	}
+	
+}
