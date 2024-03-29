e27a2747136d83f688c4c66d3e1e0a505e7a2358
Yane Wanderley <yane.rodrigues@ufpe.br>
2024-03-29 02:38:18 -0300

    Comunicação Síncrona - Preparação de Deleção Entre Microservices

--- src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
+++ src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -1,31 +1,46 @@
 package com.ead.authuser.services.impl;
 
-import com.ead.authuser.models.UserModel;
-import com.ead.authuser.repositories.UserRepository;
-import com.ead.authuser.services.UserService;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+
+import com.ead.authuser.models.UserCourseModel;
+import com.ead.authuser.models.UserModel;
+import com.ead.authuser.repositories.UserCourseRepository;
+import com.ead.authuser.repositories.UserRepository;
+import com.ead.authuser.services.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     UserRepository userRepository;
+    
+    @Autowired
+    UserCourseRepository userCourseRepository;
 
     @Override
     public List<UserModel> findAll() {
         return userRepository.findAll();
     }
 
+    @Transactional
     @Override
     public Optional<UserModel> findById(UUID userId) {
+    	List<UserCourseModel> userCourseModelList = userCourseRepository.findAllUserCourseIntoUser(userId);
+    	if(!userCourseModelList.isEmpty()) {
+    		userCourseRepository.deleteAll(userCourseModelList);
+    	}
+    	
         return userRepository.findById(userId);
     }
 
